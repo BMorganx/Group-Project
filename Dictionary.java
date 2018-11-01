0ce0c2c0bacd81b8950d0fd38042310e974b973d
Jaclyn <44209123+JaclynCao@users.noreply.github.com>
2018-10-31 19:07:52 -0500

    Update
    For the Main class I changed it so I can call the insult for yo mama, yo dad, and yourself. The output() you had made it random so it wouldn't have worked for the GUI I also deleted the other options because that would be too many buttons for the GUI.

--- Dictionary.java
+++ Dictionary.java
@@ -7,16 +7,16 @@ import java.util.Scanner;
 public class Dictionary {
 
     public ArrayList<String> adjectivesList = new ArrayList<String>();
-    public ArrayList<String> nounsList = new ArrayList<String>();
-    public ArrayList<String> verbsList = new ArrayList<String>();
+    public ArrayList<String> foodsList = new ArrayList<String>();
+    public ArrayList<String> fruitsList = new ArrayList<>();
+    public ArrayList<String> vegetablesList = new ArrayList<>();
 
-
-    /*
-    Desc: Gets a random adjective.
-    Precondition: Adjectives.txt exists.
-    Postcondition: returns a random element from the adjectivesArray
-    Throws: FileNotFoundException if the file Adjectives.txt is not found.
-    */
+    /**
+     * Desc: Gets a random adjective from adjective.txt
+     * Precondition: Adjectives.txt exists.
+     * @return randomAdjective
+     * @throws FileNotFoundException if the file Adjectives.txt is not found.
+     */
     public String adjectives() throws FileNotFoundException {
         Scanner adFile = new Scanner(new File("Adjectives.txt"));
 
@@ -31,48 +31,64 @@ public class Dictionary {
 
         return randomAdjective;
     }
-    
-      /**
-     * verbs
-     * public String verbs() throws FileNotFoundException
-     * Desc: Get a random verb for insult sentence
-     * Precondition: Verbs.txt exists.
-     * Postcondition: returns a random element from the verbsArray.
-     * Throws: FileNotFoundException if the file Verbs.txt is not found.
+
+    /**
+     * Desc: Gets a random noun from food.txt
+     * Precondition: Food.txt exists
+     * @return randomFood
+     * @throws FileNotFoundException if the file Food.txt is not found.
      */
-    public String verbs() throws FileNotFoundException {
-    	Scanner verFile = new Scanner(new File("Verbs.txt"));
-    	
-    	while(verFile.hasNext()) {
-    		verbsList.add(verFile.nextLine());
-    	}
-        
-    	String[] verbsArray = verbsList.toArray(new String[50]);
-        
-    	int index = new Random().nextInt(verbsArray.length);
-    	String randomVerb = (verbsArray[index]);
-    	
-    	return randomVerb;
-    }
+    public String foods() throws FileNotFoundException {
+        Scanner foodsFile = new Scanner(new File("Food.txt"));
 
-
-    /*
-    Desc: Gets a random noun.
-    Precondition: Nouns.txt exists.
-    Postcondition: returns a random element from the nounsArray
-    Throws: FileNotFoundException if the file Nouns.txt is not found.
-    */
-    public String nouns() throws FileNotFoundException {
-        Scanner nounsFile = new Scanner(new File("Nouns.txt"));
-
-        while(nounsFile.hasNext()){
-            nounsList.add(nounsFile.nextLine());
+        while(foodsFile.hasNext()){
+            foodsList.add(foodsFile.nextLine());
         }
 
-        String[] nounsArray = nounsList.toArray(new String[30]);
-        int index2 = new Random().nextInt(nounsArray.length);
-        String randomNoun = (nounsArray[index2]);
+        String[] foodsArray = foodsList.toArray(new String[95]);
+        int index2 = new Random().nextInt(foodsArray.length);
+        String randomFood = (foodsArray[index2]);
 
-        return randomNoun;
+        return randomFood;
+    }
+
+    /**
+     * Desc: Gets a random fruit from file Fruits.txt.
+     * Precondition: Fruits.txt exists.
+     * @return randomFruit
+     * @throws FileNotFoundException if the file Fruits.txt is not found.
+     */
+    public String fruits() throws FileNotFoundException {
+        Scanner fruitsFile = new Scanner(new File("Fruits.txt"));
+
+        while(fruitsFile.hasNext()){
+            foodsList.add(fruitsFile.nextLine());
+        }
+
+        String[] fruitsArray = fruitsList.toArray(new String[36]);
+        int index2 = new Random().nextInt(fruitsArray.length);
+        String randomfruit = (fruitsArray[index2]);
+
+        return randomfruit;
+    }
+
+    /**
+     * Desc: Gets a random vegetable from Vegetables.txt
+     * Precondition: Vegetables.txt exists.
+     * @return randomVegetables
+     * @throws FileNotFoundException if the file Vegetables.txt is not found.
+     */
+    public String vegetables() throws FileNotFoundException {
+        Scanner vegetablesFile = new Scanner(new File("Vegetables.txt"));
+
+        while(vegetablesFile.hasNext()){
+            foodsList.add(vegetablesFile.nextLine());
+        }
+
+        String[] vegetablesArray = vegetablesList.toArray(new String[24]);
+        int index2 = new Random().nextInt(vegetablesArray.length);
+        String randomVegetable = (vegetablesArray[index2]);
+
+        return randomVegetable;
     }
 }
--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
+    Dictionary dictionary = new Dictionary();
+
+
     /**
      *output
      *public String output() throws FileNotFoundException
@@ -16,33 +19,58 @@ public class Main {
 
         String insult;
         int choiceInsult = ThreadLocalRandom.current().nextInt(0, 6 + 1);
-       
+
         // Generate a random insult from cases
         switch(choiceInsult) {
-	        case 1:
-	        	insult = ("You are a/n " + dictionary.adjectives() + " " + dictionary.nouns() + "!");
-	        	break;
-	        case 2:
-	        	insult = ("Your mom is a/an" + dictionary.adjectives() + " " + dictionary.nouns() + "!");
-	        	break;
-	        case 3:
-	        	insult = ("Your dad is a/an" + dictionary.adjectives() + " " + dictionary.nouns() + "!");
-	        	break;
-	        case 4:
-	        	insult = ("Your sister is a/an" + dictionary.adjectives() + " " + dictionary.nouns() + "!");
-	        	break;
-	        case 5:
-	        	insult = ("Your computer is " + dictionary.adjectives() + " " + "!");
-	        	break;
-	        case 6:
-	        	insult = ("Your house is " + dictionary.adjectives() + " " + "!");
-	        	break;
-	        default:
-	        	insult = ("You are a/an" + dictionary.adjectives() + " " + dictionary.nouns() + "!");
-			break;
+            case 1:
+                insult = ("You are a/n " + dictionary.adjectives() + " " + dictionary.foods() + "!");
+                break;
+            case 2:
+                insult = ("Yo mama is a/an " + dictionary.adjectives() + " " + dictionary.foods() + "!");
+                break;
+            case 3:
+                insult = ("Yo dada is a/an " + dictionary.adjectives() + " " + dictionary.foods() + "!");
+                break;
+            default:
+                insult = ("You are a/an " + dictionary.adjectives() + " " + dictionary.foods() + "!");
+                break;
         }
-        
+
         return insult;
 
+    }
+
+    /**
+     * Desc: Creates insult string for "yo mama"
+     * @return insult
+     * @throws FileNotFoundException if file is not found.
+     */
+
+    public String outputMama() throws FileNotFoundException {
+        String  insult = ("Yo mama is a/an " + dictionary.adjectives() + " " + dictionary.foods() + "!");
+
+        return insult;
+    }
+
+    /**
+     * Desc: Creates insult string for "yo dad"
+     * @return insult
+     * @throws FileNotFoundException if file is not found.
+     */
+    public String outputDad() throws FileNotFoundException{
+        String insult = insult = ("Yo dad is a/an " + dictionary.adjectives() + " " + dictionary.foods() + "!");
+
+        return insult;
+    }
+
+    /**
+     * Desc: Creates insult string for yourself
+     * @return insult
+     * @throws FileNotFoundException if file is not found.
+     */
+    public String outputSelf() throws FileNotFoundException{
+        String insult = ("You're a/an " + dictionary.adjectives() + " " + dictionary.foods() + "!");
+
+        return insult;
     }
 }

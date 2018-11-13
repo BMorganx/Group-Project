ddfe01d3bbca311097c5699532adffa052cf320f
Brandi <43506683+BMorganx@users.noreply.github.com>
2018-11-12 21:26:42 -0600

    Added random and clear buttons with action listeners

--- InsultGUI.java
+++ InsultGUI.java
@@ -4,6 +4,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class InsultGUI extends Main {
     private JPanel panel1;
@@ -14,7 +16,18 @@ public class InsultGUI extends Main {
     private JButton YOMAMAButton;
     private JButton YODADButton;
     private JButton iNSUButton;
-
+    Main main = new Main();
+    private GridBagConstraints gbc_1;
+    private GridBagConstraints gbc_2;
+    private GridBagConstraints gbc_3;
+    private GridBagConstraints gbc_4;
+    private GridBagConstraints gbc_5;
+    private GridBagConstraints gbc_6;
+    private GridBagConstraints gbc_7;
+    private GridBagConstraints gbc_8;
+    private GridBagConstraints gbc_9;
+    private JButton btnRandom;
+    private JButton btnClear;
 
     public InsultGUI() {
         YOMAMAButton.addActionListener(new ActionListener() {
@@ -49,6 +62,25 @@ public class InsultGUI extends Main {
                 }
             }
         });
+        
+        btnRandom.addActionListener(new ActionListener() {
+        	@Override
+        	public void actionPerformed(ActionEvent e) {
+        		try {
+                    textArea1.setText(outputAny());
+                } catch (FileNotFoundException e1) {
+                    e1.printStackTrace();
+                }
+        		
+        	}
+        });
+        
+        btnClear.addActionListener(new ActionListener() {
+        	@Override
+        	public void actionPerformed(ActionEvent e) {
+        		textArea1.setText("");
+        	}
+        });
     }
 
     public static void main(String[] args) {
@@ -82,6 +114,7 @@ public class InsultGUI extends Main {
         textArea1.setText("");
         GridBagConstraints gbc;
         gbc = new GridBagConstraints();
+        gbc.insets = new Insets(0, 0, 5, 0);
         gbc.gridx = 0;
         gbc.gridy = 4;
         gbc.gridwidth = 3;
@@ -92,88 +125,118 @@ public class InsultGUI extends Main {
         if (label1Font != null) label1.setFont(label1Font);
         label1.setForeground(new Color(-1));
         label1.setText("Insult yourself, yo mama, or yo dada.");
-        gbc = new GridBagConstraints();
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        panel1.add(label1, gbc);
+        gbc_1 = new GridBagConstraints();
+        gbc_1.insets = new Insets(0, 0, 5, 5);
+        gbc_1.gridx = 1;
+        gbc_1.gridy = 1;
+        panel1.add(label1, gbc_1);
         final JLabel label2 = new JLabel();
         Font label2Font = this.$$$getFont$$$("Century Gothic", -1, 12, label2.getFont());
         if (label2Font != null) label2.setFont(label2Font);
         label2.setForeground(new Color(-1));
         label2.setText("You can also check what insult category you want.");
-        gbc = new GridBagConstraints();
-        gbc.gridx = 1;
-        gbc.gridy = 2;
-        panel1.add(label2, gbc);
+        gbc_2 = new GridBagConstraints();
+        gbc_2.insets = new Insets(0, 0, 5, 5);
+        gbc_2.gridx = 1;
+        gbc_2.gridy = 2;
+        panel1.add(label2, gbc_2);
         ALLFOODSCheckBox = new JCheckBox();
         ALLFOODSCheckBox.setBackground(new Color(-400419));
         ALLFOODSCheckBox.setForeground(new Color(-822925));
         ALLFOODSCheckBox.setText("ALL FOODS");
-        gbc = new GridBagConstraints();
-        gbc.gridx = 1;
-        gbc.gridy = 3;
-        panel1.add(ALLFOODSCheckBox, gbc);
+        gbc_3 = new GridBagConstraints();
+        gbc_3.insets = new Insets(0, 0, 5, 5);
+        gbc_3.gridx = 1;
+        gbc_3.gridy = 3;
+        panel1.add(ALLFOODSCheckBox, gbc_3);
         VEGETABLESCheckBox = new JCheckBox();
         VEGETABLESCheckBox.setBackground(new Color(-400419));
         VEGETABLESCheckBox.setForeground(new Color(-822925));
         VEGETABLESCheckBox.setText("VEGETABLES");
-        gbc = new GridBagConstraints();
-        gbc.gridx = 2;
-        gbc.gridy = 3;
-        gbc.anchor = GridBagConstraints.WEST;
-        panel1.add(VEGETABLESCheckBox, gbc);
+        gbc_4 = new GridBagConstraints();
+        gbc_4.insets = new Insets(0, 0, 5, 0);
+        gbc_4.gridx = 2;
+        gbc_4.gridy = 3;
+        gbc_4.anchor = GridBagConstraints.WEST;
+        panel1.add(VEGETABLESCheckBox, gbc_4);
         FRUITSCheckBox = new JCheckBox();
         FRUITSCheckBox.setBackground(new Color(-400419));
         FRUITSCheckBox.setForeground(new Color(-822925));
         FRUITSCheckBox.setText("FRUITS");
-        gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 3;
-        gbc.anchor = GridBagConstraints.WEST;
-        panel1.add(FRUITSCheckBox, gbc);
+        gbc_5 = new GridBagConstraints();
+        gbc_5.insets = new Insets(0, 0, 5, 5);
+        gbc_5.gridx = 0;
+        gbc_5.gridy = 3;
+        gbc_5.anchor = GridBagConstraints.WEST;
+        panel1.add(FRUITSCheckBox, gbc_5);
         final JLabel label3 = new JLabel();
         Font label3Font = this.$$$getFont$$$("Century Gothic", Font.BOLD, 20, label3.getFont());
         if (label3Font != null) label3.setFont(label3Font);
         label3.setForeground(new Color(-1));
-        label3.setText(" ♥ I N S U L T  G E N E R A T O R ♥");
-        gbc = new GridBagConstraints();
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        gbc.anchor = GridBagConstraints.NORTH;
-        panel1.add(label3, gbc);
+        label3.setText("\u2665 I N S U L T  G E N E R A T O R \u2665");
+        gbc_6 = new GridBagConstraints();
+        gbc_6.insets = new Insets(0, 0, 5, 5);
+        gbc_6.gridx = 1;
+        gbc_6.gridy = 0;
+        gbc_6.anchor = GridBagConstraints.NORTH;
+        panel1.add(label3, gbc_6);
         YODADButton = new JButton();
         YODADButton.setBackground(new Color(-822925));
         Font YODADButtonFont = this.$$$getFont$$$("Century Gothic", -1, -1, YODADButton.getFont());
         if (YODADButtonFont != null) YODADButton.setFont(YODADButtonFont);
+        iNSUButton = new JButton();
+        iNSUButton.setBackground(new Color(-822925));
+        Font iNSUButtonFont = this.$$$getFont$$$("Century Gothic", -1, -1, iNSUButton.getFont());
+        iNSUButton.setForeground(new Color(-1));
+        iNSUButton.setText("I N S U L T  Y O U R S E L F !");
+        gbc_9 = new GridBagConstraints();
+        gbc_9.insets = new Insets(0, 0, 5, 5);
+        gbc_9.gridx = 1;
+        gbc_9.gridy = 5;
+        gbc_9.fill = GridBagConstraints.HORIZONTAL;
+        panel1.add(iNSUButton, gbc_9);
         YODADButton.setForeground(new Color(-1));
         YODADButton.setText("YO DAD");
-        gbc = new GridBagConstraints();
-        gbc.gridx = 2;
-        gbc.gridy = 5;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel1.add(YODADButton, gbc);
+        gbc_7 = new GridBagConstraints();
+        gbc_7.insets = new Insets(0, 0, 5, 0);
+        gbc_7.gridx = 2;
+        gbc_7.gridy = 5;
+        gbc_7.fill = GridBagConstraints.HORIZONTAL;
+        panel1.add(YODADButton, gbc_7);
         YOMAMAButton = new JButton();
         YOMAMAButton.setBackground(new Color(-822925));
         Font YOMAMAButtonFont = this.$$$getFont$$$("Century Gothic", -1, -1, YOMAMAButton.getFont());
         if (YOMAMAButtonFont != null) YOMAMAButton.setFont(YOMAMAButtonFont);
         YOMAMAButton.setForeground(new Color(-1));
         YOMAMAButton.setText("YO MAMA");
-        gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 5;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel1.add(YOMAMAButton, gbc);
-        iNSUButton = new JButton();
-        iNSUButton.setBackground(new Color(-822925));
-        Font iNSUButtonFont = this.$$$getFont$$$("Century Gothic", -1, -1, iNSUButton.getFont());
+        gbc_8 = new GridBagConstraints();
+        gbc_8.insets = new Insets(0, 0, 5, 5);
+        gbc_8.gridx = 0;
+        gbc_8.gridy = 5;
+        gbc_8.fill = GridBagConstraints.HORIZONTAL;
+        panel1.add(YOMAMAButton, gbc_8);
         if (iNSUButtonFont != null) iNSUButton.setFont(iNSUButtonFont);
-        iNSUButton.setForeground(new Color(-1));
-        iNSUButton.setText("I N S U L T  Y O U R S E L F !");
-        gbc = new GridBagConstraints();
-        gbc.gridx = 1;
-        gbc.gridy = 5;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel1.add(iNSUButton, gbc);
+        
+        btnRandom = new JButton();
+        btnRandom.setText("Random");
+        btnRandom.setForeground(Color.WHITE);
+        btnRandom.setBackground(new Color(243, 113, 115));
+        GridBagConstraints gbc_btnRandom = new GridBagConstraints();
+        gbc_btnRandom.fill = GridBagConstraints.HORIZONTAL;
+        gbc_btnRandom.insets = new Insets(0, 0, 0, 5);
+        gbc_btnRandom.gridx = 0;
+        gbc_btnRandom.gridy = 6;
+        panel1.add(btnRandom, gbc_btnRandom);
+        
+        btnClear = new JButton();
+        btnClear.setText("Clear");
+        btnClear.setForeground(Color.WHITE);
+        btnClear.setBackground(new Color(243, 113, 115));
+        GridBagConstraints gbc_btnClear = new GridBagConstraints();
+        gbc_btnClear.fill = GridBagConstraints.HORIZONTAL;
+        gbc_btnClear.gridx = 2;
+        gbc_btnClear.gridy = 6;
+        panel1.add(btnClear, gbc_btnClear);
     }
 
     /**
@@ -201,4 +264,21 @@ public class InsultGUI extends Main {
     public JComponent $$$getRootComponent$$$() {
         return panel1;
     }
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
 }
